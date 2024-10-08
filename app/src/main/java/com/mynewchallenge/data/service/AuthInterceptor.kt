a840a12cb7a4e0f59e11703641f5db2dd6bb368a
sCerdan <sjcerdansantiagojoel@teco.com.ar>
2024-10-08 20:32:58 -0300

    Modeling data

---
+++ app/src/main/java/com/mynewchallenge/data/service/AuthInterceptor.kt
@@ -0,0 +1,15 @@
+package com.mynewchallenge.data.service
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+            .newBuilder()
+            .addHeader("AuthToken", "Auth-Key")
+            .build()
+
+        return chain.proceed(request = request)
+    }
+}
--- app/src/main/java/com/mynewchallenge/data/service/GithubApiService.kt
+++ app/src/main/java/com/mynewchallenge/data/service/GithubApiService.kt
@@ -1,8 +1,16 @@
 package com.mynewchallenge.data.service
 
+import com.mynewchallenge.data.model.UserDto
+import com.mynewchallenge.data.model.UsersListDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface GithubApiService {
+    @GET("/users")
+    suspend fun getAllUsers(): List<UsersListDto>
 
-
-
-
+    @GET("/users/{userName}")
+    suspend fun getUser(
+        @Path("userName") userName: String
+    ): UserDto
 }
